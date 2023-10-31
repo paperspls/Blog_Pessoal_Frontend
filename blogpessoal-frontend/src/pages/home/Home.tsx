a7ae5fbcfc7f81f0751e40dc5970ed86819869bf
Rhyan Magalhães <rjobdg@outlook.com>
2023-10-31 15:10:26 -0300

    Implementando Delete, Update e Create

--- blogpessoal-frontend/src/pages/home/Home.tsx
+++ blogpessoal-frontend/src/pages/home/Home.tsx
@@ -5,7 +5,7 @@ function Home() {
                 <div className='container grid grid-cols-2 text-white'>
                     <div className="flex flex-col gap-4 items-center justify-center py-4">
                         <h2 className='text-5xl font-bold'>
-                            Seja Bem Vinde!
+                            Olá!
                         </h2>
                         <p className='text-xl'>
                             Expresse aqui seus pensamentos e opiniões
